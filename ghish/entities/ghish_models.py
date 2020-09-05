2a99860cd33d4a503cbb9b485b0b20be35ae8d37
sk3l <mike@skelton.onl>
2020-09-05 18:00:01 -0400

    Cleanup unused modules

--- ghish/entities/ghish_models.py
+++
@@ -1,9 +0,0 @@
-from uuid import uuid1
-
-from marshmallow import Schema, fields
-
-
-class GhishOrganization(Schema):
-    _id = fields.UUID(missing=uuid1)
-    organization_name = fields.String()
-    url = fields.String()
