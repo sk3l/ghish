b84c1cbab2dc2c1378134e8102fac33a24b43f4d
sk3l <mike@skelton.onl>
2020-09-07 18:26:52 -0400

    Add comment writer to GitHubCommentSchema

--- ghish/entities/gh_comments.py
+++ ghish/entities/gh_comments.py
@@ -4,33 +4,38 @@ from marshmallow import EXCLUDE, Schema, fields, pre_load
 
 
 class GitHubCommentSchema(Schema):
-
-    def __init__(self,
-                 many=False,
-                 unknown=EXCLUDE,
-                 only=None,
-                 exclude=(),
-                 context=None,
-                 load_only=(),
-                 dump_only=(),
-                 partial=False):
-        super().__init__(many=many,
-                         unknown=unknown,
-                         only=only,
-                         exclude=exclude,
-                         context=context,
-                         load_only=load_only,
-                         dump_only=dump_only,
-                         partial=partial)
+    def __init__(
+        self,
+        many=False,
+        unknown=EXCLUDE,
+        only=None,
+        exclude=(),
+        context=None,
+        load_only=(),
+        dump_only=(),
+        partial=False,
+    ):
+        super().__init__(
+            many=many,
+            unknown=unknown,
+            only=only,
+            exclude=exclude,
+            context=context,
+            load_only=load_only,
+            dump_only=dump_only,
+            partial=partial,
+        )
 
     _id = fields.UUID(missing=uuid1)
     comment_id = fields.Integer()
     author = fields.Nested("GitHubUserSchema", required=False, unknown=EXCLUDE)
+    user = fields.Nested("GitHubUserSchema", required=False, unknown=EXCLUDE)
     create_date = fields.DateTime()
     comment_text = fields.String()
     url = fields.String()
     pull_request = fields.Nested(
-            "GitHubPullRequestSchema", required=False, unknown=EXCLUDE)
+        "GitHubPullRequestSchema", required=False, unknown=EXCLUDE
+    )
 
     @pre_load
     def map_input_fields(self, in_data, **kwargs):
